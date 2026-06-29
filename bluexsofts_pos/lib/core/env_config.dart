d99397daf0c260dad5996547866e54fcc753a493
faizanaliaslam1234-lgtm <faizanaliaslam1234@gmail.com>
2026-06-30 03:42:52 +0500

    Update API base URL to production

--- bluexsofts_pos/lib/core/env_config.dart
+++ bluexsofts_pos/lib/core/env_config.dart
@@ -4,13 +4,13 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 class EnvConfig {
   static String get apiBaseUrl {
     if (kIsWeb) {
-      return 'http://localhost:3000/api';
+      return 'https://my-pos-system-yxm9.onrender.com/api';
     }
     try {
       if (Platform.isAndroid) {
-        return 'http://10.0.2.2:3000/api';
+        return 'https://my-pos-system-yxm9.onrender.com/api';
       }
     } catch (_) {}
-    return 'http://localhost:3000/api';
+    return 'https://my-pos-system-yxm9.onrender.com/api';
   }
 }
