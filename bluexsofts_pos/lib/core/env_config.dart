c96cd7994c00ce4dc9a566b2100446365ff3283c
faizanaliaslam1234-lgtm <faizanaliaslam1234@gmail.com>
2026-07-01 16:42:14 +0500

    Fix API URL for live Render deployment

--- bluexsofts_pos/lib/core/env_config.dart
+++ bluexsofts_pos/lib/core/env_config.dart
@@ -2,16 +2,11 @@ import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 class EnvConfig {
-  static bool get _isLocalDev => const bool.fromEnvironment('LOCAL_DEV', defaultValue: true);
-
   static String get apiBaseUrl {
-    if (_isLocalDev) {
-      if (kIsWeb) return 'http://localhost:3000/api';
-      try {
-        if (Platform.isAndroid) return 'http://10.0.2.2:3000/api';
-      } catch (_) {}
-      return 'http://localhost:3000/api';
-    }
-    return 'https://my-pos-system-yxm9.onrender.com/api';
+    if (kIsWeb) return 'https://my-pos-system-yxm9.onrender.com/api';
+    try {
+      if (Platform.isAndroid) return 'http://10.0.2.2:3000/api';
+    } catch (_) {}
+    return 'http://localhost:3000/api';
   }
 }
