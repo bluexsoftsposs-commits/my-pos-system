86145b98e3f61a4a1aa5e90dab470de942b9e998
faizanaliaslam1234-lgtm <faizanaliaslam1234@gmail.com>
2026-07-01 20:28:53 +0500

    Fix backend API URL to point to correct service

--- bluexsofts_pos/lib/core/env_config.dart
+++ bluexsofts_pos/lib/core/env_config.dart
@@ -2,6 +2,6 @@ import 'package:flutter/foundation.dart';
 
 class EnvConfig {
   static String get apiBaseUrl => kIsWeb
-      ? 'https://my-pos-system-2.onrender.com/api'
-      : 'https://my-pos-system-2.onrender.com/api';
+      ? 'https://my-pos-system-2-api.onrender.com/api'
+      : 'https://my-pos-system-2-api.onrender.com/api';
 }
